f0f54b7fd91ad9e67030b057fa4e50a1dc5b7bd1
PrashanthG407 <prashanth.gopal22@gmail.com>
2025-08-13 12:08:16 -0400

    Update dueAssocLearn.ino

--- dueAssocLearn/dueAssocLearn.ino
+++ dueAssocLearn/dueAssocLearn.ino
@@ -1,5 +1,4 @@
 
-
 /*
  * Author: Joey Broussard
  * PNI, 20200820
@@ -286,7 +285,7 @@ void startSession(unsigned long now) {
 
 //Start trial
 void startTrial(unsigned long now){
-  if (!trial.trialIsRunning){
+  if (trial.trialIsRunning==false){
     trial.currentTrial += 1;
 
     trial.trialStartMillis = now;
@@ -329,9 +328,7 @@ void stopTrial(unsigned long now) {
   trial.ITI = random(trial.ITIlow,trial.ITIhigh);
   trial.ITIstartMillis = now;
   trial.ITIstillStartMillis = now;
-
-  delay(100);            // 100 ms gap with pin LOW
-  startTrial(millis());  // next trial: pin 7 goes HIGH again
+	
   
 }
 
@@ -671,20 +668,45 @@ void loop()
 	trial.inCS = (trial.msIntoTrial > trial.preCSdur) && (trial.msIntoTrial < trial.preCSdur + trial.CSdur);
   trial.inUS = (trial.msIntoTrial > trial.preCSdur + trial.CS_USinterval) && (trial.msIntoTrial < (trial.preCSdur + trial.CS_USinterval + trial.USdur));
 
+  // End of ITI -> now we finally drop pin 7 LOW, log stop, gap 100 ms, then next trial
+  if (!trial.trialIsRunning && trial.sessionIsRunning &&
+      (trial.msIntoITI>trial.ITItimeout || (trial.msIntoStillITI>trial.ITI))) {
+
+	  // 1) end the combined TRIAL+ITI block
+	 digitalWrite(trial.trialPin, LOW);
+	 serialOut(now, "stopTrial", trial.currentTrial);
+	
+	 // 2) if that was the last trial, finish the session and don't rise again
+	 if (trial.currentTrial == trial.numTrial - 1) {
+	   stopSession(now);
+	 } else {
+	   // 3) 100 ms low gap
+	   delay(100);
+	
+	   // 4) start the next trial (this sets pin 7 HIGH again)
+	   startTrial(millis());
+	 }
+   }
 
   //Updating all hardware components
   updateEncoder(now);
   updateLED(now);
   updateUS(now);
 	update2P(now);
+  
+  //Stop at end of trialDur if trialIsRunning
+  if (now > (trial.trialStartMillis + trial.trialDur) &&
+  	  trial.trialIsRunning && trial.sessionIsRunning) {
 
+    trial.trialIsRunning      = false;      // we're in ITI now
+    trial.ITI                 = random(trial.ITIlow, trial.ITIhigh);
+    trial.ITIstartMillis      = now;        // ITI timing starts
+    trial.ITIstillStartMillis = now;        // stillness timer resets
 
-  // Stop when the *whole window* (trial + ITI) is done
-  if (trial.trialIsRunning && trial.sessionIsRunning && now >= now + trial.trialDur + trial.ITI) {
-	digitalWrite(trial.trialPin, LOW);                 // falling edge at end of ITI
-	serialOut(now, "endCycle", trial.currentTrial);    
-  }  
-
+    // Note: DO NOT drive trialPin LOW here.
+    // Note: DO NOT call stopTrial() here.
+  }
+	
   if (Serial.available() > 0) {
     char startMarker = '<';
     char endMarker = '>';
